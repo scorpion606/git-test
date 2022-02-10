3a2f1547caf0c6cf723e0874b82d5231fe1d47ad
scorpion <2625618158@qq.com>
2022-02-10 21:58:56 +0800

    second commit

--- src/main/java/com/example/gittest/Gittest.java
+++ src/main/java/com/example/gittest/Gittest.java
@@ -7,5 +7,6 @@ package com.example.gittest;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git2");
     }
 }
