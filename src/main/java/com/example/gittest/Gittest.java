4da089cdbfd31843528b9ab2a13458a6fe1bf53c
scorpion <2625618158@qq.com>
2022-02-10 21:49:08 +0800

    first commit

---
+++ src/main/java/com/example/gittest/Gittest.java
@@ -0,0 +1,11 @@
+package com.example.gittest;
+
+/**
+ * @author scorpion
+ * @date 2022/2/10
+ */
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
